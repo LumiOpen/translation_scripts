cfe0f8972b88cc44186fd996a4317e81bdb10060
Maria Barrett <maria.barrett@amd.com>
2025-01-28 15:54:21 +0200

    add functionality for using DeepL instead of trying Opus, then NLLB

--- translate_mtbench.py
+++ translate_mtbench.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import math
 from comet import load_from_checkpoint
+import deepl
 
 tqdm.pandas() 
 
@@ -13,7 +14,8 @@ tqdm.pandas()
 This script translates an mt bench question file from English to a target language.
 It assumes that the text to be translated is in the column turn and references and is a flat list. 
 It checks whether an Opus model exist for the English -> target language and if not:
-use NLLB instead. It runs comet without a reference to get an extimate of the translation.
+use NLLB instead. Or you can set the --deepl flag and use DeepL for translation.
+It runs Comet without a reference to get an extimate of the translation.
 Author: Maria
 """
 
@@ -49,6 +51,13 @@ def translate_nllb(text: str, tokenizer, model, src_lang:str, tgt_lang:str) -> s
     translated_text = output[0]['translation_text']
     return translated_text
 
+def translate_deepl(text:str, tgt_lang:str, translator) -> str:
+    if len(text)<1:
+        return text
+    else:
+        result = translator.translate_text(text, target_lang=tgt_lang)
+        return(result.text)  
+
 def load_iso2nllb_map(filepath):
     iso2nllb_dict = {}
     with open(filepath, 'r') as file:
@@ -82,11 +91,13 @@ if __name__ == "__main__":
     parser.add_argument('--source-file', type=str, required=False, default='/scratch/project_462000353/maribarr/FastChat/fastchat/llm_judge/data/mt_bench/question.jsonl', help="Path to the English source file")
     parser.add_argument('--output-dir', type=str, required=True, help="Path to the output file")
     parser.add_argument('--max-samples', type=int, default=None, help="Only take top n rows")
+    parser.add_argument('--deepl', type=bool, required=False, default=False, help="Whether to use DeepL for translation")
     args = parser.parse_args()
     
     # Load the model and tokenizer
     model_name = f'Helsinki-NLP/opus-mt-en-{args.tgt_lang}'
     checkpoint = 'facebook/nllb-200-distilled-1.3B'
+    #checkpoint = 'facebook/nllb-200-distilled-600M'
     output_file = os.path.join(args.output_dir, f"question_{args.tgt_lang}.jsonl")
 
     print(f"Translating MT bench questions from en to {args.tgt_lang}")
@@ -95,7 +106,12 @@ if __name__ == "__main__":
     using_NLLB = False # flag to specify that we use NLLB
     src_lang = 'en'
     # check if the opus model exists
-    if opus_model_exists(model_name):
+    if args.deepl:
+        print("Using DeepL translator")
+        auth_key =  os.getenv('deepl_auth_key')
+        translator = deepl.Translator(auth_key)
+
+    elif opus_model_exists(model_name):
         print("Opus model found")
         tokenizer = MarianTokenizer.from_pretrained(model_name)
         model = MarianMTModel.from_pretrained(model_name)
@@ -120,12 +136,22 @@ if __name__ == "__main__":
     if args.max_samples:
         df = df.head(args.max_samples)
 
-    #Translate using Opus
-    if not using_NLLB:
+    if args.deepl:
+        df.loc[:, 'translated_turns'] = df.turns.progress_map(lambda x: [translate_deepl(text=sent,
+                                                                                        translator=translator,
+                                                                                        tgt_lang=args.tgt_lang) for sent in x]
+                                                              )
+
+        df.loc[:, 'translated_reference'] = df.reference.progress_map(lambda x: [translate_deepl(text=sent,
+                                                                                                translator=translator,
+                                                                                                tgt_lang=args.tgt_lang)
+                                                                                                for sent in x] if type(x)==list else x
+                                                                      )
+
+    elif not using_NLLB:    #Then translate using Opus
         df.loc[:, 'translated_turns'] = df.turns.progress_map(lambda x: [translate_opus(sent,
                                                                                         tokenizer=tokenizer,
                                                                                         model=model) for sent in x])
-
         df.loc[:, 'translated_reference'] = df.reference.progress_map(
             lambda x: [translate_opus(sent,
                                       tokenizer=tokenizer,
@@ -149,7 +175,7 @@ if __name__ == "__main__":
     # save to file
     #drop the english question column
     df = df.loc[:, [c for c in df.columns if c not in ["turns", 'reference']]]
-    df.rename(columns={'translated_turns': 'turns'}, inplace=True)
+    df.rename(columns={'translated_turns': 'turns', 'translated_reference': 'reference'}, inplace=True)
 
     with open(output_file, "w") as f:
         f.write(df.to_json(orient='records', lines=True, force_ascii=False))
